a238ec76ab40c08e5b0f4dec822825d272b1e640
Rauf Bayramov <raufbyrmv8@gmail.com>
2025-10-26 13:46:41 +0400

    test(user-ms): add Mockito + MockMvc tests for auth flows; add spring-kafka compile fix

--- user-ms/src/main/java/az/ingress/userms/model/dto/ForgetPasswordDto.java
+++ user-ms/src/main/java/az/ingress/userms/model/dto/ForgetPasswordDto.java
@@ -1,8 +1,12 @@
 package az.ingress.userms.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ForgetPasswordDto {
 
     private String email;
